3571dc4ffac2c0bb0cde27d694f1836fff12879b
Ashraf Khan <khan.ashraf2311@gmail.com>
2023-11-27 16:35:19 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,30 @@
+const accountId = 144335
+let accountEmail = "ashraf@gmail.com"
+var accountPassword = "12345"
+accountCity = "New York"
+
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121212"
+accountCity = "Miami"
+
+// accountId = 2
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+let myObj = {
+    name: "ashraf",
+    age: 22
+}
+
+
+const myFunction = function(){
+    console.log("Hello World");
+}
