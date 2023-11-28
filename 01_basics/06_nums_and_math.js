4f604ccaf213a6ce74a7bf832eedc92a360a402e
Ashraf Khan <khan.ashraf2311@gmail.com>
2023-11-28 12:26:02 +0000

    added arrays

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,18 @@
+const score = 400
+
+const balance = new Number(100)
+
+console.log(balance);
+
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+
+const otherNumber = 2123.8955
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString());
+
